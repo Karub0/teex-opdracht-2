77ff869468a5399cb35e0e313aa908ba18f097da
Rody Middelkoop <rody.middelkoop@han.nl>
2023-06-22 13:33:10 +0200

    Upgrade to Spring Boot 3.1.0 and moved from springfox to springdoc with OpenAPI support

--- backend/src/main/java/nl/han/se/ans/api/OpenExamQuestionController.java
+++ backend/src/main/java/nl/han/se/ans/api/OpenExamQuestionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins ={"http://localhost:3000", "http://localhost:5173"})
 @RestController
 @RequestMapping("/api")
 public class OpenExamQuestionController {
--- backend/src/main/java/nl/han/se/ans/api/SwaggerConfig.java
+++ backend/src/main/java/nl/han/se/ans/api/SwaggerConfig.java
@@ -1,38 +1,24 @@
 package nl.han.se.ans.api;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.regex("/api/.*"))
-                .build()
-                .apiInfo(apiInfo());
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("HAN SE ANSIE API")
+                        .description("Prototype back-end for ANSIE")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
     }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("TITLE")
-                .description("DESCRIPTION")
-                .version("VERSION")
-                .termsOfServiceUrl("http://terms-of-services.url")
-                .license("LICENSE")
-                .licenseUrl("http://url-to-license.com")
-                .build();
-    }
-
 }
 
